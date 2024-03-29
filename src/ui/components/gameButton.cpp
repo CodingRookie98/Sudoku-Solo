b77aaec60a93eec2ac2ef6476c3288b98cd314cd
CodingRookie <520whjoker@gmail.com>
2024-03-29 19:47:18 +0800

    feat: custom game button

---
+++ src/ui/components/gameButton.cpp
@@ -0,0 +1,46 @@
+﻿/**
+ ******************************************************************************
+ * @file           : gameButton.cpp
+ * @author         : CodingRookie
+ * @brief          : None
+ * @attention      : None
+ * @date           : 2024/3/28
+ ******************************************************************************
+ */
+
+// You may need to build the project (run Qt uic code generator) to get "ui_GameButton.h" resolved
+
+#include <QFile>
+#include "gameButton.h"
+#include "ui_GameButton.h"
+#include "soundEffects.h"
+
+GameButton::GameButton(QWidget *parent) :
+    QPushButton(parent), ui(new Ui::GameButton) {
+    ui->setupUi(this);
+
+    init();
+    signalsProcess();
+}
+
+GameButton::~GameButton() {
+    delete ui;
+}
+
+void GameButton::init() {
+    QFile styleFile(":/qss/gameButton.qss");
+    styleFile.open(QFile::ReadOnly);
+    QString style = QLatin1String(styleFile.readAll());
+    this->setStyleSheet(style);
+}
+
+void GameButton::signalsProcess() {
+    connect(this, &QPushButton::clicked, this, [] {
+        SoundEffects::getInstance()->play(SoundEffects::getInstance()->m_click_001);
+    });
+}
+
+void GameButton::enterEvent(QEnterEvent *event) {
+    SoundEffects::getInstance()->play(SoundEffects::getInstance()->m_hover_34);
+    QWidget::enterEvent(event);
+}
---
+++ src/ui/components/gameButton.h
@@ -0,0 +1,40 @@
+﻿/**
+ ******************************************************************************
+ * @file           : gameButton.h
+ * @author         : CodingRookie
+ * @brief          : None
+ * @attention      : None
+ * @date           : 2024/3/28
+ ******************************************************************************
+ */
+
+#ifndef SUDOKU_SOLO_SRC_UI_COMPONENTS_GAMEBUTTON_H_
+#define SUDOKU_SOLO_SRC_UI_COMPONENTS_GAMEBUTTON_H_
+
+#include <QPushButton>
+#include <QSoundEffect>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class GameButton;
+}
+QT_END_NAMESPACE
+
+class GameButton : public QPushButton {
+    Q_OBJECT
+
+public:
+    explicit GameButton(QWidget *parent = nullptr);
+    ~GameButton() override;
+
+protected:
+    void enterEvent(QEnterEvent *event) override;
+
+private:
+    Ui::GameButton *ui;
+
+    void init();
+    void signalsProcess();
+};
+
+#endif // SUDOKU_SOLO_SRC_UI_COMPONENTS_GAMEBUTTON_H_
