f94d1d906ec5549b7ab0fa251b27b2b8929590b0
CodingRookie <520whjoker@gmail.com>
2024-04-05 22:52:19 +0800

    feat: Add gameMessageBox

---
+++ src/ui/components/gameMessageBox.cpp
@@ -0,0 +1,45 @@
+﻿/**
+ ******************************************************************************
+ * @file           : gameMessageBox.cpp
+ * @author         : CodingRookie
+ * @brief          : None
+ * @attention      : None
+ * @date           : 2024/4/5
+ ******************************************************************************
+ */
+
+// You may need to build the project (run Qt uic code generator) to get "ui_GameMessageBox.h" resolved
+
+#include <QFile>
+#include "gameMessageBox.h"
+#include "ui_GameMessageBox.h"
+
+GameMessageBox::GameMessageBox(QWidget *parent) :
+    QDialog(parent), ui(new Ui::GameMessageBox) {
+    ui->setupUi(this);
+
+    init();
+    signalsProcess();
+}
+
+GameMessageBox::~GameMessageBox() {
+    delete ui;
+}
+
+void GameMessageBox::setMessage(const QString &message) {
+    ui->messageLabel->setText(message);
+}
+
+void GameMessageBox::init() {
+    this->setWindowFlag(Qt::FramelessWindowHint);
+    this->setModal(true);
+
+    QFile styleFile(":/qss/gameMessageBox.qss");
+    QString style(styleFile.readAll());
+    this->setStyleSheet(style);
+    this->setWindowOpacity(0.8);
+}
+
+void GameMessageBox::signalsProcess() {
+    connect(ui->btnClose, &QPushButton::clicked, this, &GameMessageBox::close);
+}
---
+++ src/ui/components/gameMessageBox.h
@@ -0,0 +1,38 @@
+﻿/**
+ ******************************************************************************
+ * @file           : gameMessageBox.h
+ * @author         : CodingRookie
+ * @brief          : None
+ * @attention      : None
+ * @date           : 2024/4/5
+ ******************************************************************************
+ */
+
+#ifndef SUDOKU_SOLO_SRC_UI_COMPONENTS_GAMEMESSAGEBOX_H_
+#define SUDOKU_SOLO_SRC_UI_COMPONENTS_GAMEMESSAGEBOX_H_
+
+#include <QDialog>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class GameMessageBox;
+}
+QT_END_NAMESPACE
+
+class GameMessageBox : public QDialog {
+    Q_OBJECT
+
+public:
+    explicit GameMessageBox(QWidget *parent = nullptr);
+    ~GameMessageBox() override;
+
+    void setMessage(const QString &message);
+
+private:
+    Ui::GameMessageBox *ui;
+
+    void init();
+    void signalsProcess();
+};
+
+#endif // SUDOKU_SOLO_SRC_UI_COMPONENTS_GAMEMESSAGEBOX_H_
