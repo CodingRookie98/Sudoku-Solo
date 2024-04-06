8400bc6883b16a001a6a392d6cd41b54a780eb91
CodingRookie <520whjoker@gmail.com>
2024-04-06 18:40:39 +0800

    feat: Add Confirmation Dialog

---
+++ src/ui/components/confirmationDialog.cpp
@@ -0,0 +1,48 @@
+﻿/**
+ ******************************************************************************
+ * @file           : confirmationDialog.cpp
+ * @author         : CodingRookie
+ * @brief          : None
+ * @attention      : None
+ * @date           : 2024/4/6
+ ******************************************************************************
+ */
+
+// You may need to build the project (run Qt uic code generator) to get "ui_ConfirmationDialog.h" resolved
+
+#include "confirmationDialog.h"
+#include "ui_ConfirmationDialog.h"
+
+ConfirmationDialog::ConfirmationDialog(QWidget *parent) :
+    QDialog(parent), ui(new Ui::ConfirmationDialog) {
+    ui->setupUi(this);
+
+    init();
+    signalsProcess();
+}
+
+ConfirmationDialog::~ConfirmationDialog() {
+    delete ui;
+}
+
+void ConfirmationDialog::init() {
+    this->setModal(true);
+    this->setWindowOpacity(0.8);
+    this->setWindowFlag(Qt::WindowType::FramelessWindowHint);
+}
+
+void ConfirmationDialog::signalsProcess() {
+    connect(ui->btnYes, &QPushButton::clicked, this, [&] {
+        emit sigYes();
+        this->close();
+    });
+
+    connect(ui->btnNo, &QPushButton::clicked, this, [&] {
+        emit sigNo();
+        this->close();
+    });
+}
+
+void ConfirmationDialog::setText(const QString &text) {
+    ui->label->setText(text);
+}
---
+++ src/ui/components/confirmationDialog.h
@@ -0,0 +1,41 @@
+﻿/**
+ ******************************************************************************
+ * @file           : confirmationDialog.h
+ * @author         : CodingRookie
+ * @brief          : None
+ * @attention      : None
+ * @date           : 2024/4/6
+ ******************************************************************************
+ */
+
+#ifndef SUDOKU_SOLO_SRC_UI_COMPONENTS_CONFIRMATIONDIALOG_H_
+#define SUDOKU_SOLO_SRC_UI_COMPONENTS_CONFIRMATIONDIALOG_H_
+
+#include <QDialog>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class ConfirmationDialog;
+}
+QT_END_NAMESPACE
+
+class ConfirmationDialog : public QDialog {
+    Q_OBJECT
+
+public:
+    explicit ConfirmationDialog(QWidget *parent = nullptr);
+    ~ConfirmationDialog() override;
+
+    void setText(const QString &text);
+signals:
+    void sigYes();
+    void sigNo();
+
+private:
+    Ui::ConfirmationDialog *ui;
+
+    void init();
+    void signalsProcess();
+};
+
+#endif // SUDOKU_SOLO_SRC_UI_COMPONENTS_CONFIRMATIONDIALOG_H_
